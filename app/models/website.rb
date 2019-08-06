bc421518d13f04f5d6abf34bd5a2a7b418eb5854
jn-aman <jn_aman@yahoo.com>
2019-08-06 16:57:26 +0530

    bug fixes

--- app/models/website.rb
+++ app/models/website.rb
@@ -20,21 +20,25 @@ end
 def rectify
 	k="https://"
 @test_string= self.url
-if @test_string.include? "http://www."
+if @test_string["http://www."]
    @test_string.slice! "http://www."
    @test_string = k+@test_string
 
+elsif @test_string["https://www."]
+@test_string.slice! "https://www."
+@test_string = k+@test_string
 
-elsif  @test_string.include? "http://"
+elsif  @test_string["http://"]
    @test_string.slice! "http://"
    @test_string=k+@test_string
 
+elsif @test_string["https://"]
 
 
-
-elsif @test_string.include? "www."
+elsif @test_string["www."]
    @test_string.slice! "www."
 @test_string = k+@test_string
+
 
 else
 	@test_string = k+@test_string
